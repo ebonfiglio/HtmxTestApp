f7bda907a72048e344dfd7420e767e4bf779631c
Evan <evanbonfiglio@gmail.com>
2023-01-25 23:18:48 -0500

    add page for teams

---
+++ HtmxTestApp/Pages/Teams/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HtmxTestApp.Pages.Teams
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
