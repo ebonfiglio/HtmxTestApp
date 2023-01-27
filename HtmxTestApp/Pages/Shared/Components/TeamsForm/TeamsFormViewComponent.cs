4246ad3ee8a96a56d8f2662717ed9ccbc6bab3b1
Evan <evanbonfiglio@gmail.com>
2023-01-26 23:53:28 -0500

    Add form component

---
+++ HtmxTestApp/Pages/Shared/Components/TeamsForm/TeamsFormViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using HtmxTestApp.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmxTestApp.Pages.Shared.Components.TeamsForm
+{
+    public class TeamsFormViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Team team)
+        {
+            if (team is null) team = new();
+            return View(team);
+        }
+    }
+}
