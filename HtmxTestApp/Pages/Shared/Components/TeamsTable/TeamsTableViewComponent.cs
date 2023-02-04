1b5923b9956292e26a43a9fba407eb35fc422b56
Evan Bonfiglio <evanbonfiglio@gmail.com>
2023-02-04 17:55:01 -0500

    Add table component

---
+++ HtmxTestApp/Pages/Shared/Components/TeamsTable/TeamsTableViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using HtmxTestApp.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmxTestApp.Pages.Shared.Components.TeamsTable
+{
+    public class TeamsTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<Team> teams)
+        {
+            if (teams is null) teams = new List<Team>();
+            return View(teams);
+        }
+    }
+}
