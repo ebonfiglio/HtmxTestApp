b0ff45dac69d0fdb8a0c798817aa0a32f37cbb2c
Evan Bonfiglio <evanbonfiglio@gmail.com>
2023-02-18 18:34:07 -0500

    Add dialog component

---
+++ HtmxTestApp/Pages/Shared/Components/FormDialog/DialogOptionsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HtmxTestApp.Pages.Shared.Components.FormDialog
+{
+    public class DialogOptionsModel
+    {
+        public string ElementId { get; set; }
+        public Guid? EntityId { get; set; }
+        public string ComponentName { get; set; }
+    }
+}
---
+++ HtmxTestApp/Pages/Shared/Components/FormDialog/FormDialogViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using HtmxTestApp.Domain.Services;
+using HtmxTestApp.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmxTestApp.Pages.Shared.Components.FormDialog
+{
+    public class FormDialogViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(DialogOptionsModel model)
+        {
+            return View(model);
+        }
+    }
+}
